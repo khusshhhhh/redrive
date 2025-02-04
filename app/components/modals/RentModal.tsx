666a35608a563fc1bfd4c9cc11aa1e79c663a9b3
khusshhhhh <workforkhush8@gmail.com>
2025-02-04 16:35:35 +1030

    Redrive15

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -327,7 +327,7 @@ const RentModal = () => {
             actionLabel={actionLabel}
             secondaryActionLabel={secondaryActionLabel}
             secondaryAction={step == STEPS.CATEGORY ? undefined : onBack}
-            title="Redrive your utilities!"
+            title="Redrive your utilities!!"
             body={bodyContent}
         />
     );
