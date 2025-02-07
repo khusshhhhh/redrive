b5d9783212d8619921cf3d5d0997acb7089e7ff7
khusshhhhh <workforkhush8@gmail.com>
2025-02-07 13:42:22 +1030

    Redrive22

--- app/components/HeartButton.tsx
+++ app/components/HeartButton.tsx
@@ -1,8 +1,8 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 'use client';
 
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 import { SafeUser } from "../types";
+import useFavorite from "../hooks/useFavorites";
 
 interface HeartButtonProps {
     listingId: string;
@@ -13,8 +13,10 @@ const HeartButton: React.FC<HeartButtonProps> = ({
     listingId,
     currentUser
 }) => {
-    const hasFavorited = false;
-    const toggleFavorite = () => { };
+    const { hasFavorited, toggleFavorite } = useFavorite({
+        listingId,
+        currentUser
+    })
     return (
         <div
             onClick={toggleFavorite}
