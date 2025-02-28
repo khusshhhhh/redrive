3f29fe884906550d5a3851869e25ee9a11b54f6e
khusshhhhh <workforkhush8@gmail.com>
2025-02-28 15:18:42 +1030

    Redrive 130 - Badge System Working. Have to add in the Listing Info

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import LoginModal from "./components/modals/LoginModal";
 import getCurrentUser from "./actions/getCurrentUser";
 import RentModal from "./components/modals/RentModal";
 import SearchModal from "./components/modals/SearchModal";
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata = {
   title: "Redrive",
@@ -35,6 +36,7 @@ export default async function RootLayout({
           <Navbar currentUser={currentUser} />
         </ClientOnly>
         <div className="pb-20 pt-28">{children}</div>
+        <Analytics />
       </body>
     </html>
   );
