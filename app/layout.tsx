d6f780aefb507baed02ad382fddb904fce111a06
heer-au <heermahida1273@gmail.com>
2025-03-03 21:56:34 +1030

    Redrive 145 by Heer

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata = {
   title: "Redrive",
-  description: "Created & Developed by Khush Patel",
+  description: "Created & Developed by Khush & Hiral Patel",
 };
 
 // ✅ Apply Poppins font
