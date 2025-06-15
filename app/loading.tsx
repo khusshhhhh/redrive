f01b7f76fb21094a341fcab4658da428fb5a20be
khusshhhhh <workforkhush8@gmail.com>
2025-06-15 11:45:29 +0930

    Redrive 177

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,7 +2,7 @@ import SkeletonCard from "./components/SkeletonCard";
 
 const Loading = () => {
   return (
-    <div className="px-2 pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 xl:grid-cols-6 2xl:grid-cols-6 gap-8">
+    <div className="px-10 pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 xl:grid-cols-6 2xl:grid-cols-6 gap-8">
       {Array.from({ length: 6 }).map((_, index) => (
         <SkeletonCard key={index} />
       ))}
